750ba7b761263807a57767e22a63424a557930d3
david <dmenjim072@g.educaand.es>
2022-11-14 14:44:11 +0100

    asd

--- src/main/java/EjemplosPOO/Vehiculo.java
+++ src/main/java/EjemplosPOO/Vehiculo.java
@@ -2,11 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package EjemploPOOVehiculo;
-
+package EjemplosPOO;
 
 public class Vehiculo {
-    
+
     private int ruedas;
     private int peso;
     private int motor;
@@ -14,12 +13,11 @@ public class Vehiculo {
     String marca;
     String modelo;
     String matricula;
-    
-    public Vehiculo(){
-        
+
+    public Vehiculo() {
+
         ruedas = 4;
         peso = 500;
         motor = 1600;
     }
 }
-
