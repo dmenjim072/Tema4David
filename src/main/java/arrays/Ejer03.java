275630bb289731186adf7cf65b35f53366681136
dmenjim072 <dmenjim072@g.educaand.es>
2022-12-05 13:16:36 +0100

    casi terminado

--- src/main/java/arrays/Ejer03.java
+++ src/main/java/arrays/Ejer03.java
@@ -4,6 +4,8 @@
  */
 package arrays;
 
+import java.util.Arrays;
+
 /**
  *
  * @author smr1d
@@ -12,16 +14,40 @@ public class Ejer03 {
     
     public static void main(String[] args) {
         
+        //Crear un array de tamaño 20
         double[] numeros = new double[20];
         
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = 7.5;
         }
         
+        //Rellenar el array con el valor 7.5 en todos sus numeros
         for (int i = 0; i < numeros.length; i++) {
-                                                
-            System.out.println("El valor de " + (i + 1) + " es: " + numeros[i]);//Lo mostramos
+                        
+            //Lo mostramos
+            System.out.println("El valor de " + (i + 1) + " es: " + numeros[i]);
+            
+            //Otra opcion
+            //Arrays.fill(numeros, 0, 20, 7.5)            
         }
+        
+        //Realiza una copia del array anterior en un nuevo array
+        double[] numeros2 = Arrays.copyOf(numeros, 20);
+        
+        
+        //Imprime ambos arrays. No se usa clase Arrays
+        System.out.println("Imprime ambos arrays");
+        
+        System.out.println("Array original");
+        for(double i : numeros){//Hacemos un bucle foreach, para recorrer el array(izq) y elegimos que array "numeros"
+            System.out.print(i + " - ");
+        }
+        
+        System.out.println("Array copia");
+        for(double i : numeros2){
+            System.out.println(i + " - ");
+        }
+                
     
     }
 }    
