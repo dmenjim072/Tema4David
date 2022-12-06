1fb6f14887e4aaca5ae1612c593a781bb3331a7f
David <dmenjim072@g.educaand.es>
2022-12-06 23:18:55 +0100

    terminado

--- src/main/java/arrays/Ejer06.java
+++ src/main/java/arrays/Ejer06.java
@@ -27,7 +27,7 @@ public class Ejer06 {
 
         //Creamos arrays para compararlos
         int array3[] = {1, 2, 3, 4};
-        int array4[] = {1, 2, 3};
+        int array4[] = {1, 2, 4};
 
         //Hacemos un  boolean para guardar la comparacion
         boolean comparacion2 = Arrays.equals(array3, array4);
@@ -45,27 +45,22 @@ public class Ejer06 {
         //Lo imprimimos para comprobar si son iguales
         System.out.println("Son iguales los arrays5 y 6?" + comparacion3);
         
-        compararArrays(array1, array2);
+        compararArrays(array1, array4);
     }
 
     //Creamos un metodo para comparar los arrays con sustitutos
     public static boolean compararArrays(int[] aux1, int[] aux2) {
-
-        boolean comparacion;
-
-        comparacion = false;//?
         
-        for (int i = 0; i < aux1.length; i++) {
-            for (int j = 0; j < aux2.length; j++) {
-                if (aux1[i] == aux2[j]) {
-                    comparacion = true;
-                    System.out.println("Son repetidos");
-                } else {
-                    comparacion = false;
-                    System.out.println("No son repetidos");
-                }
-            }
+        //Declaramos la variable Y LA INICIALIZAMOS
+        boolean comparacion = true;
+           
+           //Hacemos un bucle para que compare los valores del array uno a uno
+           for (int i = 0; i < aux1.length; i++) {
+               comparacion = aux1[i] == aux2[i];
         }
+           
+           //Lo mostramos en pantalla
+           System.out.println(comparacion);
         return comparacion;
     }
 }
