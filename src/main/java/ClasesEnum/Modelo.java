d97a617169dae6a058d5d7632772d2034abdf950
dmenjim072 <dmenjim072@g.educaand.es>
2022-12-09 11:49:34 +0100

    asd

---
+++ src/main/java/ClasesEnum/Modelo.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ClasesEnum;
+
+import java.util.Random;
+
+/**
+ *
+ * @author david
+ */
+public enum Modelo {
+    
+    SEAT_PANDA("Seat", "Panda", "48"),
+    FORD_KUGA("Ford", "Kuga", "120");
+    
+    private String marca;
+    private String modelo;
+    private String cilindrada;
+    
+    private Modelo(String marca, String modelo, String cilindrada){
+        
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cilindrada = cilindrada;
+        
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo{" + "marca=" + marca + ", modelo=" + modelo + ", cilindrada=" + cilindrada + '}';
+    }
+    
+    public static Modelo getAleatorio(){
+        
+        Random r = new Random();
+        
+        Modelo[] modelos = Modelo.values();//Devuelve el enum
+        
+        int posicionValida = r.nextInt(0, modelos.length);
+        
+        return modelos[posicionValida];
+    }
+    
+}
