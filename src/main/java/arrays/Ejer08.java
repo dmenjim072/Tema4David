e6342ed1c1e450a630f03415507b958999eda716
dmenjim072 <dmenjim072@g.educaand.es>
2022-12-15 12:46:00 +0100

    asd

---
+++ src/main/java/arrays/Ejer08.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class Ejer08 {
+    
+    public static void main(String[] args) {
+    
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Introduce un numero: ");
+        int tamaño = teclado.nextInt();
+        
+        
+        
+        System.out.println("Se va a crear un array de tamaño: " + tamaño);
+        
+        int[] tamañoArray = new int[tamaño];
+        
+        System.out.println("Introduce los valores del arrays");
+        
+    }
+    
+}
